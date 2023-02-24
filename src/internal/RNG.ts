989023ea54a062983436a6f982a7128b7e12ab4d
gasparddm <gaspard.dama@gmail.com>
2023-02-23 20:19:11 -0500

    Created a RNG in order to have a seedable RNG

---
+++ src/internal/RNG.ts
@@ -0,0 +1,38 @@
+/* ~~~~~~~~~~~~~~ */
+/* Small RNG because TS/JS do not giving a seed to Math.random().
+/* Distribution should be uniform in [0;m) ∩ N
+/* ~~~~~~~~~~~~~~ */
+
+export class RNG {
+    private previous: number;
+
+    // Equation parameters (by default the ones used by glibc)
+    private readonly a: number;
+    private readonly c: number;
+    private readonly m: number;
+
+    constructor(seed=Math.round(Math.random() * 42), a=1103515245, c=12345, m=2**31) {
+        this.previous = seed;
+        this.a = a;
+        this.c = c;
+        this.m = m;
+    }
+
+    nextInt() {
+        this.previous = (this.a * this.previous + this.c) % this.m;
+        return this.previous;
+    }
+
+    random() {
+        this.nextInt();
+        return this.previous / this.m;
+    }
+
+    inRange(left: number, right: number) {
+        if (right - left < 0 || (left == 0 && right == 0)) {
+            throw new Error("Invalid range.")
+        }
+        this.nextInt()
+        return this.previous % Math.abs(right - left)
+    }
+}
