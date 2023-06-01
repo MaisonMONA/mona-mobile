82401c2451be3e0012bc73f4b6690856668e85b3
kim841 <ktrinh7120@gmail.com>
2023-06-01 00:29:33 -0400

    calculate distance between user and discovery

--- src/views/DiscoveryDetailsPage.vue
+++ src/views/DiscoveryDetailsPage.vue
@@ -34,6 +34,7 @@
 
             <div class="discoveryDetailsContainer">
                 <div class="discoveryDetailsContent">
+                    <p>Distance: {{this.showDistance()}} km</p>
                     <!-- IF THE DISCOVERY IS AN ARTWORK -->
                     <div v-if="dType === DiscoveryEnum.ARTWORK" id="dDetails">
                         <p id="dTitle">{{ discovery.getTitle() }}</p>
@@ -101,7 +102,7 @@
                     </div>
 
                     <div class="comment-dateCreation">
-                        <p id="comment">{{ showComment() }}</p>
+                        <p id="comment">Commentaire: {{this.showComment() }}</p>
 
                     </div>
 
@@ -143,7 +144,7 @@ export default {
 
     data() {
         return {
-            cameraOutline, customMapIcon, star,
+            cameraOutline, customMapIcon, star, UserData,
             customTargetIcon: targetIconWhite  // May be overriden during mount
         }
     },
@@ -305,7 +306,39 @@ export default {
         showRating(){
             const userData = UserData.getCollected(this.discovery.id, this.discovery.dType)
             if(userData) return userData.rating
+        },
+        //Haversine Formula
+        //inspire du code python https://community.esri.com/t5/coordinate-reference-systems-blog/distance-on-a-sphere-the-haversine-formula/ba-p/902128#:~:text=For%20example%2C%20haversine(%CE%B8),longitude%20of%20the%20two%20points.
+        showDistance(){ //lng , lt
+            const lat1 = this.discovery.location.lat
+            const lat2 = UserData.getLocation()[1]
+            const lng1 = this.discovery.location.lng
+            const lng2 = UserData.getLocation()[0]
+            const R = 6371000 //radius of Earth in m
+
+            const phi1 = this.degrees2radians(lat1)
+            const phi2 = this.degrees2radians(lat2)
+
+            const delta_phi = this.degrees2radians(lat2 - lat1)
+            const delta_lambda = this.degrees2radians(lng2 - lng1)
+            const a = Math.sin(delta_phi/2.0)** 2 + Math.cos(phi1) * Math.cos(phi2) * Math.sin(delta_lambda/2.0)**2
+            const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt((1-a)))
+            const meters = R * c
+            const km = meters / 1000.0
+
+            return this.roundDown(km)
+
+        },
+        //source: https://www.w3resource.com/javascript-exercises/javascript-math-exercise-33.php
+        degrees2radians(degrees){
+            const pi = Math.PI;
+            return degrees * (pi/180);
+        },
+        //inspire de https://stackoverflow.com/questions/33429136/round-to-3-decimal-points-in-javascript-jquery
+        roundDown(number){ //up to 3 decimal
+            return Math.round(number * 1000) / 1000
         }
+
     }
 }
 </script>
