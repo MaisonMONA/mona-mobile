f3d29f71f48ad8d0f2cb16a3b7cad66005574aa9
kim841 <ktrinh7120@gmail.com>
2023-07-29 18:22:09 -0400

    move commit

---
+++ src/components/AccountContainer.vue
@@ -0,0 +1,65 @@
+<template>
+    <ion-header>
+        <ion-toolbar>
+            <ion-buttons slot="start">
+                <ion-back-button></ion-back-button>
+            </ion-buttons>
+            <ion-title>MONA</ion-title>
+        </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+        <ion-list>
+            <ion-item>
+                <ion-input label="Nom d'utilisateur" labelPlacement="stacked" placeholder="Enter text"></ion-input>
+
+
+            </ion-item>
+        </ion-list>
+
+        <ion-input label="Courriel" label-placement="floating" fill="outline" placeholder="Enter text"></ion-input>
+        <ion-input label="Mot de passe" label-placement="floating" fill="outline" placeholder="Enter text"></ion-input>
+
+    </ion-content>
+</template>
+
+
+
+<script>
+import {IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, IonInput } from "@ionic/vue";
+import { defineComponent } from 'vue';
+export default {
+    name: "AboutContainer",
+    components: {
+        IonHeader, IonContent, IonToolbar, IonButtons, IonBackButton, IonTitle, IonInput
+    }
+}
+</script>
+
+<style scoped>
+@import url("@/theme/GlobalStyle.css");
+@import url("@/theme/TopToolbar.css");
+
+.main-container {
+    padding: 5vw;
+}
+
+ion-title {
+    font-family: 'Gotham Rounded Light', sans-serif;
+}
+
+* {
+    font-family: 'Open Sans', sans-serif;
+}
+
+p {
+    margin-top: 2vw;
+    margin-bottom: 2vw;
+}
+
+a {
+    color: steelblue;
+    text-decoration: none;
+    font-weight: normal;
+}
+</style>
