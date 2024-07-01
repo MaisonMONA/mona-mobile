4df4b22cc3c2f613b996bc68afa26d403c030dc6
Sarah H <sarah.heng@umontreal.ca>
2024-07-01 17:37:33 -0400

    Changed nav bar's appearance for new interface

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -1,25 +1,30 @@
 <template>
     <ion-page>
         <ion-tabs @ionTabsWillChange="checkRoute">
-<!--        <ion-tabs>-->
+
             <ion-router-outlet></ion-router-outlet>
             <ion-tab-bar slot="bottom">
 
-                <ion-tab-button id="map" tab="map" href="/tabs/map">
-                    <ion-icon :icon="customMap"/>
+                <ion-tab-button id="map" tab="map" href="/tabs/map" :class="{ 'active-tab': activeTab === 'map' }">
+                    <ion-icon :icon="activeTab === 'map' ? activeMap : inactiveMap"/>
+                    <ion-label v-show="activeTab === 'map'">Carte</ion-label>
                 </ion-tab-button>
 
-                <ion-tab-button id="artworks-list" tab="artworks-list" href="/tabs/list">
-                    <ion-icon :icon="customList"/>
+                <ion-tab-button id="artworks-list" tab="artworks-list" href="/tabs/list" :class="{ 'active-tab': activeTab === 'artworks-list' }">
+                    <ion-icon :icon="activeTab === 'artworks-list' ? activeList : inactiveList"/>
+                    <ion-label v-show="activeTab === 'artworks-list'">Annuaire</ion-label>
                 </ion-tab-button>
 
-                <ion-tab-button id="collection" tab="collection" href="/tabs/collection">
-                    <ion-icon :icon="customCollection"/>
+                <ion-tab-button id="collection" tab="collection" href="/tabs/collection" :class="{ 'active-tab': activeTab === 'collection' }">
+                    <ion-icon :icon="activeTab === 'collection' ? activeCollection : inactiveCollection"/>
+                    <ion-label v-show="activeTab === 'collection'">Collection</ion-label>
                 </ion-tab-button>
 
-                <ion-tab-button id="more" tab="more" href="/tabs/more">
-                    <ion-icon :icon="customDotsMore"/>
+                <ion-tab-button id="more" tab="more" href="/tabs/more" :class="{ 'active-tab': activeTab === 'more' }">
+                    <ion-icon :icon="activeTab === 'more' ? activeMore : inactiveMore"/>
+                    <ion-label v-show="activeTab === 'more'">Compte</ion-label>
                 </ion-tab-button>
+
             </ion-tab-bar>
         </ion-tabs>
     </ion-page>
@@ -27,10 +32,15 @@
 
 <script lang="js">
 import { IonTabBar, IonTabButton, IonTabs, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import customMap from "@/assets/drawable/icons/uncolored_map.svg"
-import customList from "@/assets/drawable/icons/uncolored_list.svg"
-import customCollection from "@/assets/drawable/icons/uncolored_collection.svg"
-import customDotsMore from "@/assets/drawable/icons/uncolored_more.svg"
+import inactiveMap from "@/assets/drawable/icons/inactive_map_tab_icon.svg"
+import inactiveList from "@/assets/drawable/icons/inactive_list_tab_icon.svg"
+import inactiveCollection from "@/assets/drawable/icons/inactive_collection_tab_icon.svg"
+import inactiveMore from "@/assets/drawable/icons/inactive_more_tab_icon.svg"
+import activeMap from "@/assets/drawable/icons/active_map_tab_icon.svg"
+import activeList from "@/assets/drawable/icons/active_list_tab_icon.svg"
+import activeCollection from "@/assets/drawable/icons/active_collection_tab_icon.svg"
+import activeMore from "@/assets/drawable/icons/active_more_tab_icon.svg"
+
 
 export default {
     components: {
@@ -39,22 +49,16 @@ export default {
 
     data() {
         return {
-            customMap, customList, customCollection, customDotsMore, console
+            inactiveMap, inactiveList, inactiveCollection, inactiveMore,
+            activeMap, activeList, activeCollection, activeMore,
+            activeTab: "map",
+            console
         }
     },
 
     methods: {
         checkRoute(event) {
-            // Resetting all icons
-            const elements = document.querySelectorAll("ion-tab-button");
-            for (const elem of elements) {
-                if (elem.id.includes(event.tab))
-                    elem.querySelector("ion-icon").classList.remove("inactive-tab");
-                else
-                    elem.querySelector("ion-icon").classList.add("inactive-tab");
-            }
-
-            // // Setting filter on the active tab
+          this.activeTab = event.tab;
         }
     }
 }
@@ -64,25 +68,38 @@ export default {
 @import url("@/theme/TopToolbar.css");
 
 ion-tab-bar {
+    padding: 0 3% 0 5%;
+    border-top: 1px solid #D9D9D9;
     height: 10%;
-    --border: 0;
-    border: 0;
     margin-top: -1px;
-    --background: var(--toolbar-purple);
+    --background: #FFFFFF;
+}
+
+ion-tab-button {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 50%;
+  border-radius: 90px;
+  justify-content: left;
 }
 
 ion-icon {
-    font-size: 8vw;
+    font-size: 6vw;
 }
 
-/* TODO Temporary, to remove when continuing to work on navigation bar*/
-ion-icon:not(.inactive-tab) {
-  background-color: rgba(252, 231, 94, 0.89);
-  border-radius: 35%;
-  padding: 15%;
+ion-label {
+  color: black;
+  font-size: 3.7vw;
+  margin-left: 10%;
 }
 
-ion-icon.inactive-tab {
-    filter: contrast(0%);
+ion-tab-button.active-tab {
+  --background: #FADA00;
 }
+
+ion-tab-button:not(.active-tab) {
+  flex-grow: 0.6;
+}
+
 </style>
