2fb8ca95626392b579b2d4bbca8b82eb08c7d029
Sarah H <sarah.heng@umontreal.ca>
2024-10-26 15:55:46 -0400

    Implemented new interface for More Page

--- src/components/MorePageContainer.vue
+++ src/components/MorePageContainer.vue
@@ -1,21 +1,20 @@
 <template>
     <ion-page>
-        <ion-content>
-          <div id="userInfoMorePage">
-            <ion-icon id="defaultUserAvatarMorePage" :icon="defaultUserAvatar"></ion-icon>
-            <div id="userInfoTextMorePage">
-              <h1 :style="{paddingTop: memberSince? '0' : '1.1vh'}">{{ username }}</h1>
-              <h6 v-if="memberSince">Membre depuis {{ memberSince }}</h6>
-            </div>
-          </div>
-            <ion-list lines="full" class="ion-padding">
-                <ion-nav-link router-direction="forward" :component="about">
-                    <ion-item>
-                        <ion-icon id="aProposIcon" :icon="aPropos" slot="start"></ion-icon>
-                        <ion-label>À propos</ion-label>
-                        <ion-icon :icon="arrowForward" slot="end"></ion-icon>
-                    </ion-item>
-                </ion-nav-link>
+        <ion-content class="ion-padding ion-text-left">
+          <img src="/assets/drawable/mona_logo_med.png" alt="MONA logo">
+          <p id="mona_description">L'application MONA est projet libre et open source pour découvrir l'art public,
+            les lieux culturels et le patrimoine du Québec. Elle est produite par la Maison MONA,
+            un organisme à but non lucratif basé à Montréal (Québec, Canada).</p>
+            <ion-list lines="full">
+              <ion-nav-link @click="playTutorial">
+                <ion-item>
+                  <ion-icon :icon="activeList" slot="start"></ion-icon>
+
+                  <ion-label>Tutoriel</ion-label>
+                  <ion-icon :icon="arrowForward" slot="end"></ion-icon>
+                </ion-item>
+              </ion-nav-link>
+
                 <ion-nav-link router-direction="forward" :component="confidentialityPolicy">
                     <ion-item>
                         <ion-icon id="confidentialityPolicyIcon" :icon="confidentialityPolicyIcon" slot="start"></ion-icon>
@@ -23,15 +22,32 @@
                         <ion-icon :icon="arrowForward" slot="end"></ion-icon>
                     </ion-item>
                 </ion-nav-link>
-                <ion-nav-link @click="playTutorial">
-                    <ion-item>
-                        <ion-icon :icon="activeList" slot="start"></ion-icon>
-
-                        <ion-label>Tutoriel</ion-label>
-                        <ion-icon :icon="arrowForward" slot="end"></ion-icon>
-                    </ion-item>
-                </ion-nav-link>
+              <ion-nav-link router-direction="forward" :component="about">
+                <ion-item lines="none">
+                  <ion-icon id="aProposIcon" :icon="aPropos" slot="start"></ion-icon>
+                  <ion-label>À propos de la maison MONA</ion-label>
+                  <ion-icon :icon="arrowForward" slot="end"></ion-icon>
+                </ion-item>
+              </ion-nav-link>
             </ion-list>
+
+          <div id="followUsAndMediaLinks" class="ion-text-center">
+            <p>Suivez-nous!</p>
+            <div id="mediaLinks">
+            <a href="https://monamontreal.org">
+              <ion-icon :icon="`/assets/drawable/icons/website_icon.svg`"></ion-icon>
+            </a>
+            <a href='mailto: data@monamontreal.org'>
+              <ion-icon :icon="`/assets/drawable/icons/mail.svg`"></ion-icon>
+            </a>
+            <a href="https://m.facebook.com/MONA.ArtPublic/">
+              <ion-icon :icon="`/assets/drawable/icons/facebook_icon.svg`"></ion-icon>
+            </a>
+            <a href="https://www.instagram.com/mona.artpublic/">
+              <ion-icon :icon="`/assets/drawable/icons/instagram_icon.svg`"></ion-icon>
+            </a>
+          </div>
+          </div>
 
           <ion-nav-link router-direction="forward" :component="logout">
             <ion-button id="disconnectButton" fill="outline">
@@ -44,7 +60,7 @@
 </template>
 
 <script>
-import { IonPage, IonContent, IonList, IonItem, IonNavLink, IonLabel, IonIcon } from "@ionic/vue";
+import {IonPage, IonContent, IonList, IonItem, IonNavLink, IonLabel, IonIcon, IonButton} from "@ionic/vue";
 import { UserData } from "@/internal/databases/UserData";
 import AboutContainer from "@/components/AboutContainer.vue";
 import { arrowForward } from "ionicons/icons";
@@ -58,7 +74,7 @@ import confidentialityPolicyIcon from "/assets/drawable/icons/confidentiality_po
 export default {
     name: "MorePageContainer",
     components: {
-        IonContent, IonList, IonItem, IonNavLink, IonLabel, IonIcon, IonPage
+        IonContent, IonList, IonItem, IonNavLink, IonLabel, IonIcon, IonPage, IonButton,
     },
 
     setup() {
@@ -68,35 +84,9 @@ export default {
             logout: LogoutContainer,
         }
     },
-  beforeMount() {
-    const created_at = UserData.getWhenAccountCreated();
-    if (created_at) {
-      const yearDayArray = /^(\d{4})-(\d{2})-/.exec(created_at);
-      const year = yearDayArray[1];
-      const month = parseInt(yearDayArray[2]);
-      const monthsArray = [
-        "Janvier",
-        "Février",
-        "Mars",
-        "Avril",
-        "Mai",
-        "Juin",
-        "Juillet",
-        "Août",
-        "Septembre",
-        "Octobre",
-        "Novembre",
-        "Décembre",
-      ];
-      this.memberSince = monthsArray[month - 1] + " " + year;
-    }
-  },
     data() {
         return {
-            memberSince: "",
-            username: UserData.getUsername(),
             arrowForward,
-            defaultUserAvatar,
             activeList,
             aPropos,
             confidentialityPolicyIcon,
@@ -113,6 +103,37 @@ export default {
 
 <style scoped>
 @import url("@/theme/GlobalStyle.css");
+
+#mona_description {
+  margin: 0 4.6vw;
+  font-size: 3.9vw;
+  line-height: 5.8vw;
+}
+
+#followUsAndMediaLinks {
+  background: #FDF4B4;
+  border-radius: 3vw;
+  width: 92vw;
+  font-size: 5vw;
+  margin-top: 3.6vh;
+  padding: 2.6vh;
+}
+
+#mediaLinks {
+  margin-top: 1vh;
+}
+
+#mediaLinks ion-icon {
+  font-size: 12vw;
+}
+
+#mediaLinks a {
+  margin-left: 7.7vw;
+}
+
+#mediaLinks a:first-child {
+  margin-left: 0;
+}
 
 #userInfoMorePage {
   color: black;
@@ -131,6 +152,13 @@ export default {
 
 #defaultUserAvatarMorePage {
   font-size: 13vw;
+}
+
+img[alt="MONA logo"] {
+  width: 21.5vw;
+  margin-top: 5vh;
+  margin-left: 35vw;
+  margin-bottom: 2.7vh;
 }
 
 #userInfoTextMorePage h1 {
@@ -160,7 +188,7 @@ ion-icon[slot="start"] {
 }
 
 ion-list {
-  margin-top: 4vh;
+  margin-top: 2.7vh;
 }
 
 ion-item {
@@ -178,7 +206,7 @@ ion-label {
   width: 88vw;
   height: 5vh;
   position: absolute;
-  bottom: 3.7vh;
+  bottom: 2.7vh;
   left: 5.7vw;
   --background-activated : #D82727;
   --color-activated : white;
