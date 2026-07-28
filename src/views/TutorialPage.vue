7f167cdb0c24b61efe836d8594ebe28c40bf2af9
chrislng <70879967+chrislng@users.noreply.github.com>
2026-07-28 03:29:56 -0400

    Reduce tutorial title margin-bottom

--- src/views/TutorialPage.vue
+++ src/views/TutorialPage.vue
@@ -339,6 +339,12 @@ img.background-blurred {
   z-index: 1;
 }
 
+@media (max-width: 767px) {
+  .tutorialHeader {
+    margin-bottom: 3vh;
+  }
+}
+
 /* Responsive adjustments for tablet and larger screens */
 @media (min-width: 768px) {
   .tutorial_content {
